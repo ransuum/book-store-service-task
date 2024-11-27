2d5a928c4511985453abd83ef29e0db832597ffe
ransuum <dmitrenkovalera9@gmail.com>
2024-11-28 01:06:28 +0200

    dd

--- src/main/java/com/epam/rd/autocode/spring/project/controller/BookController.java
+++ src/main/java/com/epam/rd/autocode/spring/project/controller/BookController.java
@@ -3,13 +3,17 @@ package com.epam.rd.autocode.spring.project.controller;
 import com.epam.rd.autocode.spring.project.dto.BookDTO;
 import com.epam.rd.autocode.spring.project.service.BookService;
 import com.epam.rd.autocode.spring.project.util.pagging.PageConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+//@Controller
 @RestController
 @RequestMapping("/books")
 public class BookController {
@@ -21,22 +25,29 @@ public class BookController {
         this.pageConfig = pageConfig;
     }
 
-    @GetMapping
-    public ResponseEntity<Map<String, Object>> getAllBooks(@RequestParam(defaultValue = "0", required = false) Integer page,
-                                                           @RequestParam(defaultValue = "10", required = false) Integer size) {
-        return ResponseEntity.ok(pageConfig.response(bookService.getAllBooks(PageRequest.of(page, size))));
-    }
+//    @GetMapping("/home")
+//    public String viewBooksPage(
+//            @RequestParam(defaultValue = "0", required = false) Integer page,
+//            @RequestParam(defaultValue = "10", required = false) Integer size,
+//            Model model
+//    ) {
+//        Page<BookDTO> books = bookService.getAllBooks(PageRequest.of(page, size));
+//        model.addAttribute("books", books);
+//        return "home-page";
+//    }
+//
+////    @DeleteMapping("/{name}")
+////    public String deleteBookOnThyme(@PathVariable String name) {
+////        bookService.deleteBookByName(name);
+////        return "delete-book";
+////    }
+////
+////    @PostMapping
+////    public String createBookOnThyme(@RequestBody BookDTO bookDTO) {
+////        return "create-book";
+////    }
 
-    @GetMapping("/by-name/{name}")
-    public ResponseEntity<BookDTO> getBooksByName(@PathVariable String name) {
-        return ResponseEntity.ok(bookService.getBookByName(name));
-    }
-
-    @PutMapping("/{name}")
-    public ResponseEntity<BookDTO> updateBook(@PathVariable String name,
-                                              @RequestBody BookDTO bookDTO) {
-        return ResponseEntity.ok(bookService.updateBookByName(name, bookDTO));
-    }
+    ///////////////////////////////////////////////////////////////////////// without thymeleaf ->
 
     @DeleteMapping("/{name}")
     public ResponseEntity<?> deleteBook(@PathVariable String name) {
@@ -47,6 +58,23 @@ public class BookController {
     @PostMapping
     public ResponseEntity<BookDTO> createBook(@RequestBody BookDTO bookDTO) {
         return new ResponseEntity<>(bookService.addBook(bookDTO), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{name}")
+    public ResponseEntity<BookDTO> updateBook(@PathVariable String name,
+                                              @RequestBody BookDTO bookDTO) {
+        return ResponseEntity.ok(bookService.updateBookByName(name, bookDTO));
+    }
+
+    @GetMapping
+    public ResponseEntity<Map<String, Object>> getAllBooks(@RequestParam(defaultValue = "0", required = false) Integer page,
+                                                           @RequestParam(defaultValue = "10", required = false) Integer size) {
+        return ResponseEntity.ok(pageConfig.response(bookService.getAllBooks(PageRequest.of(page, size))));
+    }
+
+    @GetMapping("/by-name/{name}")
+    public ResponseEntity<BookDTO> getBooksByName(@PathVariable String name) {
+        return ResponseEntity.ok(bookService.getBookByName(name));
     }
 }
 
