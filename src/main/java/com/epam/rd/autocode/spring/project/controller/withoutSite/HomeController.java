ad0f3da7681fd5b68f574a7872351dac7f9bbcd6
ransuum <dmitrenkovalera9@gmail.com>
2024-11-28 18:52:42 +0200

    dd

--- src/main/java/com/epam/rd/autocode/spring/project/controller/withoutSite/HomeController.java
+++
@@ -1,5 +0,0 @@
-package com.epam.rd.autocode.spring.project.controller.withoutSite;
-
-public class HomeController {
-    // TODO Place your code here
-}
--- src/main/java/com/epam/rd/autocode/spring/project/util/pagging/PageConfig.java
+++ src/main/java/com/epam/rd/autocode/spring/project/util/pagging/PageConfig.java
@@ -1,9 +1,23 @@
 package com.epam.rd.autocode.spring.project.util.pagging;
 
 import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public interface PageConfig<E> {
-    Map<String, Object> response(Page<E> pages);
+@Component
+public class PageConfig<T> {
+
+    public Map<String, Object> response(Page<T> page) {
+        List<T> list = page.getContent();
+        Map<String, Object> result = new HashMap<>();
+        result.put(list.get(0).getClass().getSimpleName().toLowerCase(), list);
+        result.put("totalPages", page.getTotalElements());
+        result.put("totalElements", page.getTotalElements());
+        result.put("current_page", page.getNumber());
+
+        return result;
+    }
 }
