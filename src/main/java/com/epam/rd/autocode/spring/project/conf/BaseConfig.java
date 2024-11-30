fd846f554ede86af86f087546bc16c5529186f3d
ransuum <dmitrenkovalera9@gmail.com>
2024-11-30 20:47:48 +0200

    dd

--- src/main/java/com/epam/rd/autocode/spring/project/conf/BaseConfig.java
+++ src/main/java/com/epam/rd/autocode/spring/project/conf/BaseConfig.java
@@ -1,8 +1,10 @@
 package com.epam.rd.autocode.spring.project.conf;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,10 +15,11 @@ import java.util.Locale;
 
 @Configuration
 public class BaseConfig implements WebMvcConfigurer {
+
     @Bean
     public LocaleResolver localeResolver() {
         SessionLocaleResolver slr = new SessionLocaleResolver();
-        slr.setDefaultLocale(Locale.ENGLISH);
+        slr.setDefaultLocale(Locale.US);
         return slr;
     }
 
@@ -30,6 +33,15 @@ public class BaseConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setFallbackToSystemLocale(false);
+        return messageSource;
     }
 
     @Bean
